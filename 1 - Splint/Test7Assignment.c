2789c9f568ae5845f40410053f66aff79fdcfcc2
Sahil <shlbnsl843@gmail.com>
2019-02-09 23:40:22 +0530

    Complete Splint A1 Part 1

--- 1 - Splint/Test7Assignment.c
+++ 1 - Splint/Test7Assignment.c
@@ -8,54 +8,63 @@ typedef /*@abstract@*/ char *StringData;
 
 /*@checkedstrict@*/int ct = 5;
 
-void main() {
+void main() { // ERROR 1: function main returns void, should return int
 
     printf("Hello");
 
 }
 
 int* q1(/*@null@*/int* s) {
-    return s;
-}
+    return s; /* ERROR 2: implicitly temp storage s defined as implicitly only
+               since we are returning a pointer, it may be released memory
+               or new aliases may be created
+              */
 
-void q2_1(/*@in@*/int* s) {}
+    // ERROR 3: possibly null storage returned as non-null
+}
+void q2_1(/*@in@*/int* s) {} // ERROR 4: parameter s is not used
+// ERROR 18: function exported but not used outside Test7Assignment
 
 void q2(/*@out@*/int *s) {
-     q2_1(s);
+     q2_1(s); // ERROR 5: passed storage s not completely defined
 }
 
 void q3() {
   char c;
   int i = 44;
-  c = i;
+  c = i; // ERROR 6: assignment of int to char
 }
 
 void q4(StringData p) {
-    char *s = (char*)p;
+    char *s = (char*)p; // ERROR 7: variable s declared but not used
 }
 
 int* q5() {
 
 int * x = (int *) malloc(10);
 free(x);
-return x;
+return x; // ERROR 8: Possibly null storage x returned as non-null
+          // ERROR 9: variable x used after being released
 
 }
 
 void q6(char/*@out@*/ *x,char *y) {
-    strcpy(x,y);
+    strcpy(x,y); /* ERROR 10: Parameter 1(x) to function strcpy is declared
+                      unique but may be aliased externally by parameter 2 (y) 
+
+                */
 }
 
 void q7(int *x, int *y)
 /*@modifies *x@*/
 {
-*y = *x;
+*y = *x; // ERROR 11: Undocumented modification of *y
 }
 
 
 void q8_1() {
     int x = 3;
-    while(x!=0)
+    while(x!=0) // ERROR 12: suspected infinite loop
         printf("X");
 }
 
@@ -64,8 +73,8 @@ void q8_2(int x) {
 switch (x) {
 
     case 0: printf("0");
-    case 1: printf("1");
-    default:printf("default");
+    case 1: printf("1"); // ERROR 13: fall through case, no preceding break
+    default:printf("default"); // ERROR 14: fall through case, no preceding break
     }
 }
 
@@ -78,7 +87,15 @@ void q9_1() {
 
 void q10() {
     int x = 3;
-    int y = 4 * add(x++);
+    int y = 4 * add(x++); /* ERROR 15, 16: expression has undefined behaviour
+                            This is because of the ill-defined macro add
+                            add(x) should be defined as (x) + (x)
+                            otherwise currently it is 'x + x'
+                            so add(x++) = x++ + x++
+                            The value x will be changed by either left or 
+                            the right operand  
+                          */
+    // ERROR 17: variable y declared but not used
 }
 
 
--- 1 - Splint/Test8Assignment.c
+++ 1 - Splint/Test8Assignment.c
@@ -6,75 +6,85 @@
 
 typedef /*@abstract@*/ char *StringData;
 
-/*@checkedstrict@*/int ct = 5;
-int *gp;
+/*@checkedstrict@*/int ct = 5; // ERROR 17: variable exported but not used outside the file
+int *gp; // ERROR 18: variable exported but not used outside the file
 
-void main() {
+void main() { // ERROR 1: function main should return int
 
     printf("Hello");
 
 }
 
 void q1(/*@null@*/char * s) {
-    strcpy(s,"shivanshu");
+    strcpy(s,"shivanshu"); // ERROR 2: possbily null storage passed as non-null param
 
 }
 
 void q2() {
      int q;
-     printf("Value of q is : %d", q);
+     printf("Value of q is : %d", q); // ERROR 3: variable q used before definition
 }
 
 void q3() {
   enum val{A,B,C};
-  enum val p = 5;
+  enum val p = 5; // ERROR 4: variable p declared but not used
 }
 
 void q4(StringData p) {
     char *s;
-    strcpy(p,s);
+    strcpy(p,s); // ERROR 5: variable s used before definition
 }
 
 void q5() {
 
-char * str = (char *) malloc(15);
+char * str = (char *) malloc(15); // ERROR 7: variable str declared but not used
 
-}
+} /* ERROR 6: fresh storage str not released before return
+    str is allocated memory but not released the memory before returning from
+    the function
+    */
 
 void q6(int/*@out@*/ *x)/*@modifies gp@*/ {
 gp = x;
 
-}
-
+} // ERROR 8: out storage x not defined before return
+// ERROR 9: function return with global gp not completely defined
 
 void q7_2() {
- ct = 3;
+ ct = 3; // ERROR 10: undocumented use of global ct
 }
 
 
 void q8_3() {
 int x =3,y=3;
-x == y;
+x == y; /* ERROR 11: statement has no effect, x == y, this returns 
+            a boolean value, true or false which is not stored
+            anywhere
+        */
 }
 
-int q8_4_1() {
+int q8_4_1() { // ERROR 19: function exported but not used outside the file
  return 3;
 }
 
 void q8_4_2() {
- q8_4_1();
+ q8_4_1(); // ERROR 12: return value (type int) ignored
 }
 
 
 void q9_2(char* s) {
      char *tmp = (char *)malloc(10);
-     strcpy(tmp,s);
+     strcpy(tmp,s); // ERROR 13: possibly null storage 'tmp' passed as non-null param
 
-}
+} // ERROR 14: fresh storage 'temp' not release before return
 
 void q10_2() {
 
-int ct = 3;
+int ct = 3; /* ERROR 15: variable ct shadows outer declaration
+                ct was globally declared to be 5 and now it 
+                is being modified
+            */
+// ERROR 16: variable ct declared but not used
 }
 
 
